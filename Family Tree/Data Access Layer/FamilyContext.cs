324ab29439e63911ab63b4094b56cf9476a061e0
EdgarMendoza <edgarmendoza91@gmail.com>
2015-05-21 19:54:08 -0500

    Added entities and relationships

---
+++ Family Tree/Data Access Layer/FamilyContext.cs
@@ -0,0 +1,21 @@
+﻿using Family_Tree.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family_Tree.Data_Access_Layer
+{
+    class FamilyContext : DbContext
+    {
+        public FamilyContext() : base("FamilyContext") { }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<ImmediateFamily> Family { get; set; }
+
+
+    }
+}
---
+++ Family Tree/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family_Tree.Models
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime Death { get; set; }
+        public string Gender { get; set; }
+        public string Birthplace { get; set; }
+        public bool Deceased { get; set; }
+
+        public virtual ICollection<ImmediateFamily> Family { get; set; }
+    }
+
+    enum Gender
+    {
+        Male, Female
+    }
+}
