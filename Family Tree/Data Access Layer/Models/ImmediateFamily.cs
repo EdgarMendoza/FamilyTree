238ca8aee82a6bba9de548a9748913981d3b97db
EdgarMendoza <edgarmendoza91@gmail.com>
2015-05-29 18:06:32 -0500

    Update Entity Framework and Project Structure

---
+++ Family Tree/Data Access Layer/Models/ImmediateFamily.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family_Tree.Models
+{
+    class ImmediateFamily
+    {
+        public ImmediateFamily()
+        {
+            
+        }
+
+        public int ImmediateFamilyID { get; set; }
+        public int DadRefID { get; set; }
+        public int MomRefID { get; set; }
+
+        [ForeignKey("DadRefID")]
+        public virtual Person Dad { get; set; }
+        [ForeignKey("MomRefID")]
+        public virtual Person Mom { get; set; }
+
+        public virtual ICollection<Person> Members { get; set; }
+    }
+}
